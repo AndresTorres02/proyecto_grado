4ba490e99d3ba75c3ca16ca0c3a48221745c0f27
Andres Torres <andres.torres02@uceva.edu.co>
2025-06-02 15:53:43 -0500

    Se acomodo la clase login.py para que inicie sesion con un corroe y contraseña existente de la base de datos

--- flet/login.py
+++ flet/login.py
@@ -20,16 +20,27 @@ def login_view(page: ft.Page, go_to_registrar, go_to_menu):
         resultado = cursor.fetchone()
 
         if resultado:
-            page.dialog = ft.AlertDialog(title=ft.Text("¡Bienvenido!"), on_dismiss=lambda e: go_to_menu())
-            page.dialog.open = True
+            # Puedes mostrar un mensaje de bienvenida opcional
+            page.snack_bar = ft.SnackBar(
+                ft.Text("¡Bienvenido!", color="white"),
+                bgcolor="green"
+            )
+            page.snack_bar.open = True
             page.update()
+
+            # Redirigir directamente
+            go_to_menu()
         else:
-            page.snack_bar = ft.SnackBar(ft.Text("Correo o contraseña incorrectos", color="white"), bgcolor="red")
+            page.snack_bar = ft.SnackBar(
+                ft.Text("Correo o contraseña incorrectos", color="white"),
+                bgcolor="red"
+            )
             page.snack_bar.open = True
             page.update()
 
         cursor.close()
         conn.close()
+
 
     titulo = ft.Text("Ingresar", size=30, color="black", weight=ft.FontWeight.BOLD)
     correo = ft.TextField(label="Escribe tu correo", text_style=ft.TextStyle(color="black"), border_color="black")
