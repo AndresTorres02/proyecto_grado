73a96195cf244d07db1a5538f40387b8873d9c68
Andres Torres <andres.torres02@uceva.edu.co>
2025-06-12 13:08:55 -0500

    Cambios en la clase main

--- flet/main.py
+++ flet/main.py
@@ -40,7 +40,6 @@ def main(page: ft.Page):
             page.views.append(abrir_camara(page, lambda e: page.go("/menu")) 
                              )
 
-<<<<<<< HEAD
             page.views.append(menu_principal(
                 page,
                 go_to_info=lambda: page.go("/info"),  # ir a la vista de info detallada
@@ -52,8 +51,6 @@ def main(page: ft.Page):
 
             # Vista del menú principal con opción de ir a la información detallada
             #page.views.append(menu_principal(page, go_to_info=lambda: page.go("/info")))
-=======
->>>>>>> cf4b9c552fc8961a8cfe5910ffccbb3deec59926
         elif page.route == "/info":
             # Vista de información detallada del cultivo
             page.views.append(info_detallada(page))
